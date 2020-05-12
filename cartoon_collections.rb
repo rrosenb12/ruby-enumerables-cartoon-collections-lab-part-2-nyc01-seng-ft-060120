4a2f19c6ad07e1d8aab3400dd5b6680cd73357c9
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-12 15:09:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def long_planeteer_calls(planeteer_calls)
   # Return the boolean result of this check
   planeteer_calls.map do |calls|
     if calls.size > 4
-      return calls
+      return true
     end
   end
 end
